3cc13f787c15249b7ffea50478487be7dff31047
Sanjeevani1306 <82940922+Sanjeevani1306@users.noreply.github.com>
2022-10-13 15:26:15 +0530

    Add files via upload

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,13 @@
+import 'package:user_profile_example/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://images.unsplash.com/photo-1641948598832-81c0db0ead86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+    name: 'Hrithik Roshan',
+    email: 'sanjeevanilikes@gmail.com',
+    about:
+        'The most handsome guy',
+    isDarkMode: false,
+  );
+}
